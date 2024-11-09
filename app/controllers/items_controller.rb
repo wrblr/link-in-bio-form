a78cbf227bb36f08a49b7c68a03f4ce50a2ec4c5
wrblr <wrbell@uchicago.edu>
2024-11-09 17:55:17 +0000

    final functioning version

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,19 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def display_form
+    
+    render({ :template => "item_templates/backdoor_form"})
+  end
+
+  def create_item
+    i = Item.new
+    i.link_url = params.fetch("query_link_url")
+    i.link_description = params.fetch("query_link_description")
+    i.thumbnail_url = params.fetch("query_thumbnail_url")
+    i.save
+
+    redirect_to("/")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+
+  get("/backdoor", { :controller => "items", :action => "display_form"})
+
+  post("/insert_item", { :controller => "items", :action => "create_item" })
 end
